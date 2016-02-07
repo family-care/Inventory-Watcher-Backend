33dcb9e37f22269cdceec0c5e6e297b2c133b1f9
pjozsef <pollak.jozsef1@gmail.com>
2016-02-07 17:41:30 +0100

    Json arg constructor test for Notification.

--- src/test/java/com/github/inventorytracker/model/NotificationTest.java
+++ src/test/java/com/github/inventorytracker/model/NotificationTest.java
@@ -1,6 +1,5 @@
 package com.github.inventorytracker.model;
 
-
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
@@ -13,8 +12,9 @@ import java.time.LocalDate;
 import static org.junit.Assert.assertEquals;
 
 public class NotificationTest {
+
     @BeforeClass
-    public static void init(){
+    public static void init() {
         Json.mapper.registerModule(new JavaTimeModule());
     }
 
@@ -22,10 +22,10 @@ public class NotificationTest {
     public void testPojoToString() {
         String test = Json.encode(
                 Notification.builder()
-                        .on(LocalDate.parse("2007-12-03"))
-                        .repeatInterval(3)
-                        .unit(DateUnit.DAY)
-                        .build());
+                .on(LocalDate.parse("2007-12-03"))
+                .repeatInterval(3)
+                .unit(DateUnit.DAY)
+                .build());
         String expected = new JsonObject()
                 .put("on", new JsonArray("[2007,12,3]"))
                 .put("repeatInterval", 3)
@@ -38,10 +38,10 @@ public class NotificationTest {
     public void testStringToPojo() {
         Notification test = Json.decodeValue(
                 new JsonObject()
-                        .put("on", new JsonArray("[2007,12,3]"))
-                        .put("repeatInterval", 3)
-                        .put("unit", "DAY")
-                        .encode(),
+                .put("on", new JsonArray("[2007,12,3]"))
+                .put("repeatInterval", 3)
+                .put("unit", "DAY")
+                .encode(),
                 Notification.class);
         Notification expected = Notification.builder()
                 .on(LocalDate.parse("2007-12-03"))
@@ -50,10 +50,20 @@ public class NotificationTest {
                 .build();
         assertEquals(expected, test);
     }
+
+    @Test
+    public void testJsonArgConstructor() {
+        Notification test = new Notification(
+                new JsonObject()
+                .put("on", new JsonArray("[2007,12,3]"))
+                .put("repeatInterval", 3)
+                .put("unit", "DAY")
+        );
+        Notification expected = Notification.builder()
+                .on(LocalDate.parse("2007-12-03"))
+                .repeatInterval(3)
+                .unit(DateUnit.DAY)
+                .build();
+        assertEquals(expected, test);
+    }
 }
-
-
-
-
-
-
