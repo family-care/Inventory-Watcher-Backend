7ee992184057c7654a14148703e99e41c9a8ba8e
pjozsef <pollak.jozsef1@gmail.com>
2016-02-09 00:20:18 +0100

    Added toJsonObject method.

--- src/main/java/com/github/inventorytracker/model/JsonConvertable.java
+++ src/main/java/com/github/inventorytracker/model/JsonConvertable.java
@@ -1,6 +1,7 @@
 package com.github.inventorytracker.model;
 
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 
 /**
  * This interface act as a trait, enabling a class to be JSON convertable.
@@ -10,4 +11,8 @@ public interface JsonConvertable {
     default String toJson(){
         return Json.encode(this);
     };
+    
+    default JsonObject toJsonObject(){
+        return new JsonObject(this.toJson());
+    }
 }
