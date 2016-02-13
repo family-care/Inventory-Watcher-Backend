b35b43c3e8d0d38196dee583608f20f6f8615571
pjozsef <pollak.jozsef1@gmail.com>
2016-02-14 00:46:54 +0100

    ItemVerticle added.

---
+++ src/main/java/com/github/inventorywatcher/verticle/ItemVerticle.java
@@ -0,0 +1,31 @@
+package com.github.inventorywatcher.verticle;
+
+import com.github.inventorywatcher.dao.EmbeddedMongo;
+import com.github.inventorywatcher.service.ItemService;
+import com.github.inventorywatcher.service.impl.ItemServiceImpl;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.serviceproxy.ProxyHelper;
+
+public class ItemVerticle extends AbstractVerticle{
+    private final Logger log = LoggerFactory.getLogger(DeployVerticle.class);
+    private EmbeddedMongo mongo;
+
+    @Override
+    public void start(){
+        mongo = new EmbeddedMongo(vertx, 2500, null, () -> {
+            ItemService itemService = new ItemServiceImpl(
+                    vertx,
+                    new JsonObject().put("host", "localhost").put("port", 2500)
+            );
+            ProxyHelper.registerService(ItemService.class, vertx, itemService, "database-service-address");
+        });
+    }
+
+    @Override
+    public void stop(){
+        mongo.stop();
+    }
+}
