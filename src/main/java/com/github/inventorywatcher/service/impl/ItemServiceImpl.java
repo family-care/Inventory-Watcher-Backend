bfacedcbc388c93ac4016de55f52530dd78748ca
pjozsef <pollak.jozsef1@gmail.com>
2016-02-14 09:20:45 +0100

    ItemServiceImpl edit, to conform to ServiceProxy rules.

--- src/main/java/com/github/inventorywatcher/service/impl/ItemServiceImpl.java
+++ src/main/java/com/github/inventorywatcher/service/impl/ItemServiceImpl.java
@@ -8,37 +8,56 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+
 import java.util.List;
 
 /**
- *
  * @author pjozsef
  */
 public class ItemServiceImpl implements ItemService {
     private final Vertx vertx;
     private final ItemDao dao;
-//todo specify config object
+
+    //todo specify config object
     public ItemServiceImpl(Vertx vertx, JsonObject config) {
-        this.vertx=vertx;
+        this.vertx = vertx;
         dao = ItemDao.create(vertx, config);
     }
 
     @Override
     public void getItems(Handler<AsyncResult<List<Item>>> handler) {
-        dao.getItems(handler);
+        dao.getItems(res -> {
+            if (res.succeeded()) {
+                handler.handle(Future.succeededFuture(res.result()));
+            } else {
+                handler.handle(Future.failedFuture(res.cause()));
+            }
+        });
     }
 
     @Override
     public void getItem(String id, Handler<AsyncResult<Item>> handler) {
-        dao.getItem(id, handler);
+        dao.getItem(id, res -> {
+            if (res.succeeded()) {
+                handler.handle(Future.succeededFuture(res.result()));
+            } else {
+                handler.handle(Future.failedFuture(res.cause()));
+            }
+        });
     }
 
     @Override
     public void createItem(Item item, Handler<AsyncResult<String>> handler) {
         List<String> errors = item.validate();
-        if(errors.isEmpty()){
-            dao.createItem(item, handler);
-        }else{
+        if (errors.isEmpty()) {
+            dao.createItem(item, res -> {
+                if (res.succeeded()) {
+                    handler.handle(Future.succeededFuture(res.result()));
+                } else {
+                    handler.handle(Future.failedFuture(res.cause()));
+                }
+            });
+        } else {
             handler.handle(Future.failedFuture(String.join("\n", errors)));
         }
     }
@@ -46,16 +65,28 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public void updateItem(String id, Item item, Handler<AsyncResult<Void>> handler) {
         List<String> errors = item.validate();
-        if(errors.isEmpty()){
-            dao.updateItem(id, item, handler);
-        }else{
+        if (errors.isEmpty()) {
+            dao.updateItem(id, item, res -> {
+                if (res.succeeded()) {
+                    handler.handle(Future.succeededFuture(res.result()));
+                } else {
+                    handler.handle(Future.failedFuture(res.cause()));
+                }
+            });
+        } else {
             handler.handle(Future.failedFuture(String.join("\n", errors)));
         }
     }
 
     @Override
     public void removeItem(String id, Handler<AsyncResult<Void>> handler) {
-        dao.removeItem(id, handler);
+        dao.removeItem(id, res -> {
+            if (res.succeeded()) {
+                handler.handle(Future.succeededFuture(res.result()));
+            } else {
+                handler.handle(Future.failedFuture(res.cause()));
+            }
+        });
     }
 
 }
